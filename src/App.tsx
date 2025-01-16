c135433313d702688acd2e95d84fcce361c194b2
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2025-01-16 13:37:20 +0100

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto my-10">
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
+
+export default App;
